742103f7f463b757568ae0019717f90f33d082d6
Mohammad Tukur <tukurmohammad8@gmail.com>
2026-07-29 02:33:05 +0100

    refactor(web): Tabler icons in shadcn primitives
    Swap select/dialog chevrons and close icons to Tabler; set iconLibrary
    to tabler in components.json for future shadcn adds.

--- apps/web/src/components/ui/dialog.tsx
+++ apps/web/src/components/ui/dialog.tsx
@@ -2,7 +2,7 @@
 
 import * as React from 'react';
 import * as DialogPrimitive from '@radix-ui/react-dialog';
-import { X } from 'lucide-react';
+import { IconX } from '@tabler/icons-react';
 import { cn } from '@/lib/utils';
 
 const Dialog = DialogPrimitive.Root;
@@ -38,7 +38,7 @@ const DialogContent = React.forwardRef<
     >
       {children}
       <DialogPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 hover:opacity-100">
-        <X className="h-4 w-4" />
+        <IconX className="h-4 w-4" />
         <span className="sr-only">Close</span>
       </DialogPrimitive.Close>
     </DialogPrimitive.Content>
